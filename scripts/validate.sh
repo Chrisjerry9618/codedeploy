589818699fb361a03597214cb423c647484d5a9d
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-12 09:40:19 +0000

    test

---
+++ scripts/validate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CODEDEPLOY_APPLICATION=chris-github-new
+CODEDEPLOY_DEPLOYMENTGROUP=chris-github-2
+BUCKET=testncode
+HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
+if [ "$HTTP_CODE" -eq "200" ]
+  echo "Successfully pulled root page." >> /tmp/test.txt
+  exit 0;
+else
+  app=$(aws deploy list-deployments --application-name $CODEDEPLOY_APPLICATION  --deployment-group-name  $CODEDEPLOY_DEPLOYMENTGROUP --include-only-status Succeeded --region us-east-1 --output text | head -1 | awk '{print $2}')
+  echo $app
+  KEY=$(aws deploy get-deployment --deployment-id $app --query deploymentInfo.revision.s3Location.key --region us-east-1)
+  echo $KEY
+  ROLLBACK_DEPLOYMENT=`aws deploy create-deployment --application-name ${CODEDEPLOY_APPLICATION} --deployment-config-nam CodeDeployDefault.OneAtATime --deployment-group-name ${CODEDEPLOY_DEPLOYMENTGROUP} --s3-location bucket=${BUCKET},bundleType=zip,key=${KEY} --region us-east-1 --output text`
+  echo "Redeploy happened" >> /tmp/output.txt
+  fi
