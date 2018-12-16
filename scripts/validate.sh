d107fd1bb27e13d4f0d4129a56409617073fff30
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-16 14:12:58 +0000

    testcommit3

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-CODEDEPLOY_APPLICATION=chris-github-2
-CODEDEPLOY_DEPLOYMENTGROUP=chris-github2-dep
-BUCKET=testncode
 HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
 echo $HTTP_CODE
 if [ "$HTTP_CODE" -eq "200" ];
 then
 	echo deployment succceded with successcode 200 >> /root/success.txt
 else
-	sh /opt/chris/redeploy.sh
+	sh /opt/chris/scripts/redeploy.sh
 fi
 	
