4a4ced03d138b6dbc289c5d09c4c9877d1ad881c
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-16 13:36:55 +0000

    testcheck1

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -8,9 +8,6 @@ if [ "$HTTP_CODE" -eq "200" ];
 then
 	echo deployment succceded with successcode 200 >> /root/success.txt
 else
-	app=$(aws deploy list-deployments --application-name $CODEDEPLOY_APPLICATION  --deployment-group-name  $CODEDEPLOY_DEPLOYMENTGROUP --include-only-status Succeeded --region us-east-1 --output text | head -1 | awk '{print $2}')
-	KEY=$(aws deploy get-deployment --deployment-id $app --query deploymentInfo.revision.s3Location.key --region us-east-1)
-	ROLLBACK_DEPLOYMENT=$(aws deploy create-deployment --application-name ${CODEDEPLOY_APPLICATION} --deployment-config-nam CodeDeployDefault.OneAtATime --deployment-group-name ${CODEDEPLOY_DEPLOYMENTGROUP} --s3-location bucket=${BUCKET},bundleType=zip,key=${KEY} --region us-east-1 --output text | head -1 | awk '{print $2}')
-	echo Redeploy happened >> /root/redeploy.txt
+	sh /opt/chris/redeploy.sh
 fi
 	
