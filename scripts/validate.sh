d4ff2e04ab9ade77d9f273c230f3356df0cef8e8
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-16 06:35:16 +0000

    failtest

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -12,6 +12,6 @@ else
   echo $app
   KEY=$(aws deploy get-deployment --deployment-id $app --query deploymentInfo.revision.s3Location.key --region us-east-1)
   echo $KEY
-  ROLLBACK_DEPLOYMENT=`aws deploy create-deployment --application-name ${CODEDEPLOY_APPLICATION} --deployment-config-nam CodeDeployDefault.OneAtATime --deployment-group-name ${CODEDEPLOY_DEPLOYMENTGROUP} --s3-location bucket=${BUCKET},bundleType=zip,key=${KEY} --region us-east-1 --output text`
+  ROLLBACK_DEPLOYMENT=$(aws deploy create-deployment --application-name ${CODEDEPLOY_APPLICATION} --deployment-config-nam CodeDeployDefault.OneAtATime --deployment-group-name ${CODEDEPLOY_DEPLOYMENTGROUP} --s3-location bucket=${BUCKET},bundleType=zip,key=${KEY} --region us-east-1 --output text | head -1 | awk '{print $2}')
   echo "Redeploy happened" >> /tmp/output.txt
   fi
