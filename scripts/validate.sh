ec69195accc06a29d35766ac114c68b929b9646e
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-19 10:50:33 +0000

    testcheck6

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
-HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80/xx`
+HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80/`
 echo $HTTP_CODE
 if [ "$HTTP_CODE" -eq "200" ];
 then
 	echo deployment succceded with successcode 200 >> /root/success.txt
 else
-	sh /opt/chris/scripts/redeploy.sh
+
+	exit 1 
 fi
 	
