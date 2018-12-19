dd02e716b3614b163e54367f86c5442ea1842d2c
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-19 10:04:30 +0000

    failcheck4

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,7 +5,9 @@ BUCKET=testncode
 app=`aws deploy list-deployments --application-name $CODEDEPLOY_APPLICATION  --deployment-group-name  $CODEDEPLOY_DEPLOYMENTGROUP --include-only-status Succeeded --region us-east-1 --output text | head -1 | awk '{print $2}'`
 echo $app
 KEY=`aws deploy get-deployment --deployment-id $app --region us-east-1 --query deploymentInfo.revision.s3Location.key`
-echo $KEY
-ROLLBACK_DEPLOYMENT=$(aws deploy create-deployment --application-name ${CODEDEPLOY_APPLICATION} --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name ${CODEDEPLOY_DEPLOYMENTGROUP} --s3-location bucket=${BUCKET},bundleType=zip,key=${KEY} --region us-east-1 --output text)
+#KEY="${KEY#\"}"
+KEY1=$(echo $KEY | tr -d '"')
+echo $KEY1
+ROLLBACK_DEPLOYMENT=$(aws deploy create-deployment --application-name ${CODEDEPLOY_APPLICATION} --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name ${CODEDEPLOY_DEPLOYMENTGROUP} --s3-location bucket=${BUCKET},bundleType=zip,key=${KEY1} --region us-east-1 --output text)
 echo $ROLLBACK_DEPLOYMENT
 echo "Redeploy happend" >> /root/rep.txt
