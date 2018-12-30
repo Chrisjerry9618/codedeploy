ab33a6b88e95453b62fe2d537844d5a5820a6ad5
Chrisjerry9618 <dannychris9618@gmail.com>
2018-12-30 11:40:29 +0000

    chnages with success build

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80/xxx`
+HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80/`
 echo $HTTP_CODE
 if [ "$HTTP_CODE" -eq "200" ];
 then
